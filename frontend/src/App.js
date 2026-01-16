1cdaf0070a1b97ffce8b1ff4407aa389079198e2
Amann-Raj <amanraj1227@gmail.com>
2026-01-16 15:43:04 +0530

    Test Vercel auto deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>🚀 MERN CI/CD Demo</h1>
+      <h1>🚀 MERN CI/CD Live</h1>
       <p>GitHub Actions Pipeline Working</p>
+      <p>My name is Aman Raj</p>
     </div>
   );
 }
